14a2fe0832332b6e74b3056b9de2e6616a5bad1c
Ganneya Naik Gaonkar <ganeygaonkar@gmail.com>
2023-02-27 18:08:13 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("pic1").onmouseover = function(){
+    document.displaypic.src = "img/pic1.jpg"
+}
+
+document.getElementById("pic2").onmouseover = function(){
+    document.displaypic.src = "img/pic2.jpg"
+}
+
+document.getElementById("pic3").onmouseover = function(){
+    document.displaypic.src = "img/pic3.jpg"
+}
+
+document.getElementById("pic4").onmouseover = function(){
+    document.displaypic.src = "img/pic4.jpg"
+}
+
+document.getElementById("pic5").onmouseover = function(){
+    document.displaypic.src = "img/pic5.jpg"
+}
+
+document.getElementById("pic6").onmouseover = function(){
+    document.displaypic.src = "img/pic6.jpg"
+}
+
+document.getElementById("grid").onmouseout = function(){
+    document.displaypic.src = "img/pic1.jpg"
+}
